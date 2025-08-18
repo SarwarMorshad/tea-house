49c0c2ed81cd3255091f8f2bfea898c1a46282b6
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-18 11:04:24 +0200

    Banner Section Complete

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// This is a Tailwind CSS configuration file that sets up the basic structure for a Tailwind project.
